711cd33168f1b2ec935b52a3d40529cf90e7ecba
kareemAbd0 <kareemabdo.7781@yahoo.com>
2022-08-21 16:38:09 +0200

    test folder

---
+++ Array List/cmake-build-debug/CMakeFiles/Array_List.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Array_List"
+  "Array_List.pdb"
+  "CMakeFiles/Array_List.dir/main.c.o"
+  "CMakeFiles/Array_List.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Array_List.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Array List/cmake-build-debug/CMakeFiles/Array_List.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Array_List.
+# This may be replaced when dependencies are built.
